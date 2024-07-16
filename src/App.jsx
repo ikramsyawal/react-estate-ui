63e37e4965ee124431d7db09ac5b5059af2ee9ca
Ikram Syawal Alitu <56379280+ikramsyawal@users.noreply.github.com>
2024-07-16 11:50:06 +0800

    save 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import "./layout.scss";
+import Navbar from "./components/navbar/Navbar";
+import HomePage from "./pages/homepage/HomePage";
+
 function App() {
   return (
-    <div>Hello World</div>
-  )
+    <>
+      <div className="layout">
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <div className="content">
+          <HomePage />
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/homepage/HomePage.jsx
@@ -0,0 +1,14 @@
+import "./homepage.scss";
+
+function HomePage() {
+  return (
+    <div className="homePage">
+      <div className="contentContainer">content</div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="herobg" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
